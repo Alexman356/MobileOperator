cd9414fddc50e2649af61251a5afbb2abad2f669
Vanous <https://github.com/Vanomaster/blum-blum-shuba-algorithm.git>
2022-11-12 22:04:54 +0700

    fix: delete metadata path

--- DbModels/DataBaseMobileOperator2022.Designer.cs
+++ DbModels/DataBaseMobileOperator2022.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\Alex\Desktop\Study\Курсовая работа БСБД\MobileOperator WPF\MobileOperator\DataBaseMobileOperator2002.edmx" включено. 
+﻿// Создание кода T4 для модели "C:\Users\Alex\Desktop\Study\Курсовая работа БСБД\MobileOperator WPF\MobileOperator\DataBaseMobileOperator2022.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
