477e6a9bb12c880d19f2769973d6fbd35a074968
Alex <marsik20020313@gmail.com>
2022-11-13 19:33:33 +0700

    functional: add password encryption class

---
+++ Encryption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace MobileOperator
+{
+    internal class Encryption
+    {
+        public string PasswordEncrypt(string password, string salt)
+        {
+            var sha256 = System.Security.Cryptography.SHA256.Create();
+            var saltedPasswordAsBytes = Encoding.UTF8.GetBytes($"{salt}{password}");
+
+            return Convert.ToBase64String(sha256.ComputeHash(saltedPasswordAsBytes));
+        }
+    }
+}
