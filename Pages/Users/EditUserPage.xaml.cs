eef29d4e47dce4e8f5157e16c29ef58017ed8c98
Alex <marsik20020313@gmail.com>
2022-11-15 04:35:53 +0700

    feat: add user page

---
+++ Pages/Users/EditUserPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MobileOperator
+{
+    public partial class EditUserPage : Page
+    {
+        private User CurrentUser;
+        public EditUserPage(User selectedUser)
+        {
+            InitializeComponent();
+            CurrentUser = selectedUser;
+            DataContext = selectedUser;
+        }
+        
+        private void BtnSaveUserClick(object sender, RoutedEventArgs e)
+        {
+            CheckingEnteredData();
+            
+            try
+            {
+                Context.Get().SaveChanges();
+                MessageBox.Show("Пользователь отредактирован!");
+                NavigationService.Navigate(new UserPage());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void BtnBackUserClick(object sender, RoutedEventArgs e)
+        {
+            Context.Set(null);
+            NavigationService.Navigate(new UserPage());
+        }
+
+        private void CheckingEnteredData()
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(CurrentUser.Login))
+            {
+                errors.AppendLine("Incorrect login value entered!");//TODO Fix
+            }
+
+            if (CurrentUser.Role != "Администратор" ||
+                CurrentUser.Role != "Оператор" ||
+                CurrentUser.Role != "Абонент")//TODO Fix
+            {
+                errors.AppendLine("Incorrect value entered!");
+            }
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+        }
+    }
+}
---
+++ Pages/Users/UserPage.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MobileOperator
+{
+    public partial class UserPage : Page
+    {
+        public UserPage()
+        {
+            InitializeComponent();
+            DGUser.ItemsSource = Context.Get().Users.ToList();
+        }
+
+        private void BtnEditUserClick(object sender, RoutedEventArgs e)
+        {
+            if (DGUser.SelectedItem != null)
+            {
+                NavigationService.Navigate(new EditUserPage((User)DGUser.SelectedItem));
+            }
+            else
+            {
+                MessageBox.Show("Выберите пользователя");
+            }
+        }
+
+        private void CmbBoxUserSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (CmbBoxUser.SelectedIndex != -1)
+            {
+                TxtBoxUser.IsEnabled = true;
+            }
+        }
+
+        private void TxtBoxSearchUser(object sender, TextChangedEventArgs e)
+        {
+            var text = TxtBoxUser.Text.ToLower();
+
+            SearchUser(text);
+        }
+
+        private void SearchUser(string text)
+        {
+            foreach (var user in Context.Get().Users)
+            {
+                ChangeRowVisible(user, RowIsContainsText(user, text));
+            }
+        }
+
+        private bool RowIsContainsText(User user, string text)
+        {
+            switch (CmbBoxUser.SelectedIndex)
+            {
+                case 0:
+                {
+                    return user.Login.Contains(text);
+                }
+
+                default:
+                    return false;
+            }
+        }
+
+        private void ChangeRowVisible(User row, bool isToShow)
+        {
+            var itemContainer = GetItemContainer(row);
+
+            if (!(itemContainer is DataGridRow dataGridRow))
+            {
+                return;
+            }
+
+            switch (isToShow)
+            {
+                case true:
+                    dataGridRow.Visibility = Visibility.Visible;
+                    break;
+                case false:
+                    dataGridRow.Visibility = Visibility.Collapsed;
+                    break;
+            }
+        }
+
+        private DependencyObject GetItemContainer(User row)
+        {
+            var userForHide = Context.Get().Users
+                .Where(login => login.Login == row.Login)
+                .ToList()
+                .FirstOrDefault();
+            var itemContainer = DGUser.ItemContainerGenerator
+                .ContainerFromItem(userForHide);
+
+            return itemContainer;
+        }
+    }
+}
